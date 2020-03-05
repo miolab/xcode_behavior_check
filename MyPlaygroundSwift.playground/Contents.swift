63bf44a34d982b25e1750d7783d292dc20360c9d
im <poson_hemp@yahoo.co.jp>
2020-03-05 15:50:30 +0900

    add Swift grammar: collection

--- MyPlaygroundSwift.playground/Contents.swift
+++ MyPlaygroundSwift.playground/Contents.swift
@@ -24,4 +24,25 @@ type(of: intFromStr)
 let insertString:String = "Hello and " + helloIm + " Bye."
 let insertInt:String = "The number \(number) is 3."
 
-// collection
+// collection: array
+var arrNum = [10, 20, 30]
+var arrDouble:[Double] = [1.1, 2,2]
+var arrIm:[String] = []
+arrIm = ["im_red", "im_blue", "im_green"]
+arrIm[0]
+arrIm[1...2]
+arrIm.count
+arrIm.append("im_black")
+arrIm.insert("im_gold", at:1)
+
+// collection: dict
+var dictNum = ["one":"orenge", "two":"apple", "three":"banana"]
+var dictSetting:[String:Int] = [:]
+dictSetting = ["Math":80, "English":85]
+var dictIm:[Int:String] = [0:"im_1", 1:"im_2"]
+dictSetting["Math"]
+            // Index search unuseful: (e.g.)dictSetting[0] ...error
+dictIm[0]   // as key.
+dictIm.count
+dictIm[2] = "im_10"
+dictIm
