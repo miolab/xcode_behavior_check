defb540357ec83b7f48959dab1f00ce2152e9ed9
im <poson_hemp@yahoo.co.jp>
2020-03-05 13:46:44 +0900

    Init commit

---
+++ MyPlaygroundSwift.playground/Contents.swift
@@ -0,0 +1,15 @@
+// Playground behavior check
+import UIKit
+
+var str = "Hello, playground"   // String
+var str_im = "Hello, im."
+var number = 1 + 2              // int
+
+let num = 10 + 11
+
+
+
+
+
+
+
